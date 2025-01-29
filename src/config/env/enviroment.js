2543ff47eaa3a0379091a19e82506acee0e21aaf
aagarcia-se <angelgarciap.dev@gmail.com>
2025-01-29 10:38:13 -0600

    agregar centario prueba fork

--- src/config/env/enviroment.js
+++ src/config/env/enviroment.js
@@ -11,24 +11,39 @@ export const enviroment = {
     server: {
       port: process.env.SERVER_DEV || 3000,
     },
-    smsConfig:{
+    smsConfig: {
       sid_twilio: process.env.ACCOUNTSID_DEV,
-      token_twilio: process.env.AUTHTOKEN_DEV
-    }
+      token_twilio: process.env.AUTHTOKEN_DEV,
+    },
+  },
+  pil: {
+    database: {
+      bd_url: process.env.DB_URL_PIL,
+      bd_token: process.env.DB_TOKEN_PIL,
+      sid_twilio: process.env.ACCOUNTSID_PIL,
+      token_twilio: process.env.AUTHTOKEN_PIL,
+    },
+    server: {
+      port: process.env.SERVER_PROD || 3000,
+    },
+    smsConfig: {
+      sid_twilio: process.env.ACCOUNTSID_PROD,
+      token_twilio: process.env.AUTHTOKEN_PROD,
+    },
   },
   prod: {
     database: {
       bd_url: process.env.DB_URL_PROD,
       bd_token: process.env.DB_TOKEN_PROD,
       sid_twilio: process.env.ACCOUNTSID_PROD,
-      token_twilio: process.env.AUTHTOKEN_PROD
+      token_twilio: process.env.AUTHTOKEN_PROD,
     },
     server: {
       port: process.env.SERVER_PROD || 3000,
     },
-    smsConfig:{
+    smsConfig: {
       sid_twilio: process.env.ACCOUNTSID_PROD,
-      token_twilio: process.env.AUTHTOKEN_PROD
-    }
+      token_twilio: process.env.AUTHTOKEN_PROD,
+    },
   },
 };
