1ac4461d5b3e8669014c7de735a097bc41fcd88d
aagarcia-se <angelgarciap.dev@gmail.com>
2025-03-14 21:42:52 -0600

    fix stock routes

--- src/modules/enviarsms/enviarsms.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { sendSMSController } from "./enviarsms.controller.js";
-import authMiddleware from "../../middlewares/authMiddleware.js";
-
-export const smsRoute = Router();
-
-smsRoute.post("/send-sms", authMiddleware, sendSMSController);
