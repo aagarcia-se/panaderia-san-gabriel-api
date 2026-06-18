bb07a030e296e1d563410fdcd216754ac171faf6
agarcia <angelgarciap.dev@gmail.com>
2026-06-18 00:04:20 -0600

    servicio para reconocimiento de imagenes

--- src/modules/ventasAI/ventasAI.service.js
+++ src/modules/ventasAI/ventasAI.service.js
@@ -2,7 +2,7 @@ import { procesarImagenOcrService } from "../ocr/ocr.service.js";
 
 export const IngresarVentasAIService = async (ventaHeader, image) => {
   try {
-    
+    //se agregarob apis keys
     const textoRaw = await procesarImagenOcrService(image);
 
     console.log(textoRaw);
