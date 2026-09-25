2c60f79be2d73d1444048a4f40a72a1eda5a0596
aagarcia <angelgarciap.dev@gmail.com>
2026-09-25 12:14:13 -0600

    Agregar debug

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import app from "./app.js";
 import { config } from "./config/index.js";
+import testBetterStack from "./observability/testBetterStack.js";
 
-export const startServer = () => {
-  app.listen(config.server.port, () => {
-    console.log(`Server is running`);
-  });
+export const startServer = async () => {
+  try {
+    await testBetterStack();
+
+    app.listen(config.server.port, () => {
+      console.log(
+        `Server is running on port ${config.server.port}`
+      );
+    });
+  } catch (error) {
+    console.error(
+      "Error while starting server:",
+      error
+    );
+  }
 };
