a401e2e5121f6d08dc3b03fc162f89b2242da103
aagarcia <angelgarciap.dev@gmail.com>
2026-09-25 10:02:09 -0600

    Debug

--- src/observability/config/observability.config.js
+++ src/observability/config/observability.config.js
@@ -1,14 +1,7 @@
 import { config } from "../../config/index.js";
 
 const configLog = config.observability;
-console.log("OBSERVABILITY CONFIG", {
-  environment: configLog.environment,
-  application: configLog.application,
-  service: configLog.service,
-  logLevel: configLog.logLevel,
-  hasSourceToken: Boolean(configLog.betterStack.sourceToken),
-  hasIngestingHost: Boolean(configLog.betterStack.ingestingHost),
-});
+
 const observabilityConfig = {
   environment: configLog.environment || "development",
   application: configLog.application|| "unknown",
